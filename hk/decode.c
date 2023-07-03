ea91beb7c909dedb9ee667291d63fcdfa27c5a51
searchwit <94544499+searchwit@users.noreply.github.com>
2023-07-03 17:16:24 -0400

    Update decode.c

--- hk/decode.c
+++ hk/decode.c
@@ -1,3 +1,9 @@
 INSERT INTO CTR_REF_TRANSACTION_TYPE VALUES ('USDDP', 0, 1, 'USD DRAFT PURCHASE','NEGOTIABLE_INSTRUMENT_IN');
 
 
+
+spring.datasource.url=jdbc:oracle:thin:@ofacdb_medium?TNS_ADMIN=/home/e06836/wallet/
+spring.datasource.username=flink
+spring.datasource.password=Chtian200644201
+spring.datasource.driver-class-name=oracle.jdbc.OracleDriver
+
