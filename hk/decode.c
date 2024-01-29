c6d5410e18618b2c0ec883395b8c742ffa4794dd
searchwit <94544499+searchwit@users.noreply.github.com>
2024-01-29 11:27:53 -0500

    Update decode.c

--- hk/decode.c
+++ hk/decode.c
@@ -1,6 +1,7 @@
 INSERT INTO CTR_REF_TRANSACTION_TYPE VALUES ('USDDP', 0, 1, 'USD DRAFT PURCHASE','NEGOTIABLE_INSTRUMENT_IN');
 
-
+hex:676f6f6479656172323031394023  enc:e1f3a28659bb4d1c  dec:nnnnk
+hex:676f6f6479656172323031394023  enc:3f2fab5a28fb432b031cb401f87c0ed8  dec:789
 
 spring.datasource.url=jdbc:oracle:thin:@ofacdb_medium?TNS_ADMIN=/home/e06836/wallet/
 spring.datasource.username=flink
